ee2dacf502dc8073bed082a810fd702de8958bae
Suheng Tan <530249138@qq.com>
2021-08-26 19:15:16 +0800

    100 percent complete

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class PrintAndHide : MonoBehaviour
 {
     // Start is called before the first frame update
-    int i = 4;
+    int i = 3;
     public Renderer rend;
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(gameObject.name + ":" + i++);
+        i++;
+        Debug.Log(gameObject.name + ":" + i);
+
+        if (gameObject.tag == "Red"){
+            if (i == 100)
+            {
+                gameObject.SetActive(false);  
+            }
+        }
+
+        if (gameObject.tag == "Blue")
+        {
+            if (i == Random.Range(200,251))
+            {
+                rend.enabled = false;
+            }
+        }
     }
 }
